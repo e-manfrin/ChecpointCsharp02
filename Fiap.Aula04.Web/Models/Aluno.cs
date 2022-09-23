1be9d990836d7806ad32a42ac1507f9014a398d0
e-manfrin <rm87198@fiap.com>
2022-09-23 19:48:52 -0300

    Cp

---
+++ Fiap.Aula04.Web/Models/Aluno.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Fiap.Aula04.Web.Models
+{
+    public class Aluno
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Telefone { get; set; }
+
+        [Display(Name = "Sexo")]
+        public Sexo? Sexo { get; set; }
+        public string? Curso { get; set; }
+    }
+
+    public enum Sexo
+    {
+        Feminino, Masculino, Outros
+    }
+}
